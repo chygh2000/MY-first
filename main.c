1a7494705449edb65eb7da478e71ebcdccba5d01
chygh2000 <57820987+chygh2000@users.noreply.github.com>
2020-11-14 20:05:54 +0800

    上传一个文件
    第一次使用GitHub上传文件

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+int A[10];
+int main()
+{
+void f (int num);
+   int num=0,n=0;
+    scanf("%d",&num);
+
+    return 0;
+
+}
+void f (int num){
+    s
+}
